6bbc69c55e49f5815244a4020f21fbc6d3625329
hamzaMahdi <hamzamahdi96@gmail.com>
2019-02-10 22:46:18 -0500

    lidar stopping code

---
+++ capstone_rosws/src/drive_wheelchair/src/danger_checker.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Bool
+import numpy
+pub = rospy.Publisher("danger_flag",Bool,queue_size=1)
+
+def multi_range(l, *args):
+	output = []
+	for indices in args:
+		output += l[indices[0]:indices[1]]
+		return output
+def check_obstacles(data):
+	flag = Bool()
+	flag = False
+	front = [data.ranges[i] for i in range(0,90)]
+	front +=[data.ranges[i] for i in range(650,720)]
+	#print front
+	avg = numpy.mean(front)
+	if (avg>1):
+		flag = True
+	else :
+		flage  = False
+	pub.publish(flag)
+rospy.init_node('danger_checker', anonymous=True)
+rospy.Subscriber("/scan", LaserScan, check_obstacles,queue_size=1)
+rospy.spin()
